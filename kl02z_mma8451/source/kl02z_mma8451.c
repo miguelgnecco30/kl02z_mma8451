8a74a677e739f2b3bdcfaa3f726ba9869b8caacf
Miguel Gnecco <mimagne97@gmail.com>
2021-01-16 11:33:23 -0500

    Archivos del profesor

--- kl02z_mma8451/source/kl02z_mma8451.c
+++ kl02z_mma8451/source/kl02z_mma8451.c
@@ -1,37 +1,14 @@
-/*
- * Copyright 2016-2021 NXP
- * All rights reserved.
+/*! @file : main.c
+ * @author  Miguel Gnecco
+ * @version 1.0.0
+ * @date    16/01/2021
+ * @brief   Archivo principal
+ * @details
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
-/**
- * @file    kl02z_mma8451.c
- * @brief   Application entry point.
- */
+*/
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,14 +16,39 @@
 #include "clock_config.h"
 #include "MKL02Z4.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
 
-/* TODO: insert other definitions and declarations here. */
 
-/*
- * @brief   Application entry point.
- */
+#include "sdk_hal_gpio.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Private Prototypes
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Private Source Code
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Public Source Code
+ ******************************************************************************/
 int main(void) {
+	status_t resultado;
 
   	/* Init board hardware. */
     BOARD_InitBootPins();
@@ -59,6 +61,13 @@ int main(void) {
 
     PRINTF("Hello World\n");
 
+    //coloca el pin PTB7 en alto
+    resultado=gpioPutLow(KPTB7);
+
+    if(resultado!=kStatus_Success)
+    	printf("error de operacion");
+
+
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
     /* Enter an infinite loop, just incrementing a counter. */
@@ -70,3 +79,4 @@ int main(void) {
     }
     return 0 ;
 }
+
