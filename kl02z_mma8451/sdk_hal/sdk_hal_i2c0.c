aa1bec4c3d8d7267163e050e7b20b08542aa6d4f
Miguel Gnecco <mimagne97@gmail.com>
2021-01-26 21:33:51 -0500

    Modificacion de SDK .c y .h para la lectura de los datos de mayor bits

--- kl02z_mma8451/sdk_hal/sdk_hal_i2c0.c
+++ kl02z_mma8451/sdk_hal/sdk_hal_i2c0.c
@@ -51,7 +51,7 @@ status_t i2c0MasterInit(uint32_t baud_rate) {
 	return(kStatus_Success);
 }
 /*--------------------------------------------*/
-status_t i2c0MasterReadByte(uint8_t *data, uint8_t device_address, int8_t memory_address) {
+status_t i2c0MasterReadByte(uint16_t *data, uint8_t device_address, int8_t memory_address) {
 	i2c_master_transfer_t masterXfer;
 	uint8_t i2c_rx_buffer[1];
 
@@ -71,16 +71,16 @@ status_t i2c0MasterReadByte(uint8_t *data, uint8_t device_address, int8_t memory
 }
 /*--------------------------------------------*/
 status_t i2c0MasterWriteByte(uint8_t data, uint8_t direccion){
-//	/* subAddress = 0x01, data = g_master_txBuff - write to slave.
-//	 start + slaveaddress(w) + subAddress + length of data buffer + data buffer + stop*/
-//	masterXfer.slaveAddress = I2C_MASTER_SLAVE_ADDR_7BIT;
-//	masterXfer.direction = kI2C_Write;
-//	masterXfer.subaddress = (uint32_t) deviceAddress;
-//	masterXfer.subaddressSize = 1;
-//	masterXfer.data = g_master_txBuff;
-//	masterXfer.dataSize = 1;
-//	masterXfer.flags = kI2C_TransferDefaultFlag;
-//
-//	I2C_MasterTransferBlocking(I2C0, &masterXfer);
-	return(kStatus_Success);
+/* subAddress = 0x01, data = g_master_txBuff - write to slave.
+	 start + slaveaddress(w) + subAddress + length of data buffer + data buffer + stop
+	masterXfer.slaveAddress = I2C_MASTER_SLAVE_ADDR_7BIT;
+	masterXfer.direction = kI2C_Write;
+	masterXfer.subaddress = (uint32_t) deviceAddress;
+	masterXfer.subaddressSize = 1;
+	masterXfer.data = g_master_txBuff;
+	masterXfer.dataSize = 1;
+	masterXfer.flags = kI2C_TransferDefaultFlag;
+
+	I2C_MasterTransferBlocking(I2C0, &masterXfer);
+	return(kStatus_Success);*/
 }
